d180057c45a98e870a2d4d8553c8a63b6be2b0d7
Раис Аглиуллов <raisagliullov@MacBook-Pro-Rais.local>
2020-07-09 21:52:26 +0400

    End first homework

--- FirstTestWorkAgliullov/ViewController.swift
+++ FirstTestWorkAgliullov/ViewController.swift
@@ -9,51 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    internal let firstFloat: Float? = 3.0
-    internal let secondFloat: Float? = nil
-    
-    internal var arrayInt = [18,7,3,65,12,4,9,2,73,10]
-    
-    internal var stringTask: String = "abc123"
+    //---Задание 2
+    fileprivate let a: Double = 5.0
+    fileprivate let b: Double = 7.0
+    fileprivate var avarage: Double = 0.0
+    //---Задание 3
+    fileprivate let fullName: (String, String) = ("firstName", "lastName")
+    //---Задание 4
+    fileprivate let firstFloat: Float? = 3.0
+    fileprivate var secondFloat: Float?
+    //---Задание 6
+    fileprivate var arrayInt = [18, 7, 3, 65, 12, 4, 9, 2, 73, 10]
+    //---Задание 7
+    fileprivate var trashString: String = "abc123"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        createTwoDouble(a: 5, b: 7)
-        createCortege(firstName: "Ivan", lastName: "Ivanov")
-        createTwoFloat(statement: firstFloat)
-        createTwoFloat(statement: secondFloat)
-        createFibonacciSeries()
-        bubbleSort(arrayInt)
-        stringFormatter(stringTask: stringTask)
+        self.halfSummTwoNumber()
+        self.printFullName()
+        self.printOptionalNumber(number: self.firstFloat)
+        self.printOptionalNumber(number: self.secondFloat)
+        self.printFibonacciNumber()
+        self.bubbleSort(self.arrayInt)
+        self.replaceLastCharacterString(trashString: self.trashString)
     }
     
     //MARK: - Задание 2
-    func createTwoDouble(a:Double, b:Double) {
-        let a: Double = a
-        let b: Double = b
-        let avarage = (a + b) / 2
+    func halfSummTwoNumber() {
+        self.avarage = (self.a + self.b) / 2
         print(avarage)
     }
     
     //MARK: - Задание 3
-    func createCortege(firstName: String, lastName: String) {
-        let userInfo: (String, String) = (firstName: firstName, lastName: lastName)
-        let fullName = userInfo.0 + " \(userInfo.1)"
+    func printFullName() {
+        let fullName: String = "Full name: \(self.fullName.0)" + " \(self.fullName.1)"
         print(fullName)
     }
     
     //MARK: - Задание 4
-    func createTwoFloat(statement: Float?) {
-        guard statement != nil else {
-            return print("Variable can't be unwrapped")
-        }
-        print(statement!)
+    func printOptionalNumber(number: Float?) {
+        guard let number = number else { return print("Variable can't be unwrapped") }
+        print("Number: \(number)")
     }
     
     //MARK: - Задание 5
-    func createFibonacciSeries() {
+    func printFibonacciNumber() {
         var x: Int = -1
         var y: Int = 1
         var sum: Int = 0
@@ -68,29 +69,28 @@ class ViewController: UIViewController {
     }
     
     //MARK: - Задание 6
-    func bubbleSort(_ arrayInt: [Int]) -> [Int] {
-        var array = arrayInt
-        for _ in 0...array.count {
-            for value in 1...array.count - 1 {
-                if array[value-1] > array[value] {
-                    let largerValue = array[value-1]
-                    array[value-1] = array[value]
-                    array[value] = largerValue
+    func bubbleSort(_ arrayInt: [Int]) {
+        var initialArray = arrayInt
+        for _ in 0...initialArray.count {
+            for value in 1...initialArray.count - 1 {
+                if initialArray[value - 1] > initialArray[value] {
+                    let largerValue = initialArray[value - 1]
+                    initialArray[value - 1] = initialArray[value]
+                    initialArray[value] = largerValue
                 }
             }
         }
-        print("Отсортированный массив: \(array)")
-        return array
+        print("Отсортированный массив: \(initialArray)")
     }
     
     //MARK: - Задание 7
-    func stringFormatter(stringTask: String) {
-        //var a = stringTask.last
-        if let number = Int(stringTask.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
-            let changeNumber = number + 1
-            let newString = stringTask.dropLast(3)
-            print("\(newString)\(changeNumber)")
+    func replaceLastCharacterString(trashString: String) {
+        guard let lastCharacter = trashString.last?.description, let lastNumber = Int(lastCharacter) else {
+            return print("Can't serialize last character in number")
         }
+        let numberPlusOne: Int = lastNumber + 1
+        let replaceString: String = String(trashString.dropLast()) + "\(numberPlusOne)"
+        print(replaceString)
     }
 }
 
